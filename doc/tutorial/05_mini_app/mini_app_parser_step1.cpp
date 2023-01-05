4550dccf5ea3eea7981d72177611c98a99082356
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2023-01-05 14:28:08 +0100

    tmp

--- doc/tutorial/05_mini_app/mini_app_parser_step1.cpp
+++ doc/tutorial/05_mini_app/mini_app_parser_step1.cpp
@@ -1,4 +1,5 @@
-#include <seqan3/argument_parser/all.hpp>
+#include <sharg/parser.hpp>                     // for the SeqAn sharg::parser
+
 #include <seqan3/core/debug_stream.hpp>
 
 void run_program(std::filesystem::path const & reference_path, std::filesystem::path const & index_path)
@@ -13,42 +14,65 @@ struct cmd_arguments
     std::filesystem::path index_path{"out.index"};
 };
 
-void initialise_argument_parser(seqan3::argument_parser & parser, cmd_arguments & args)
+void initialise_argument_parser(sharg::parser & parser, cmd_arguments & args)
 {
     parser.info.author = "E. coli";
-    parser.info.short_description = "Creates an index over a reference.";
+    parser.info.short_description = "Creates a space-efficient pre-filter.";
     parser.info.version = "1.0.0";
-    parser.add_option(args.reference_path,
-                      'r',
-                      "reference",
-                      "The path to the reference.",
+
+    parser.add_option(args.input_path,
+                      'i',
+                      "input",
+                      "The path to the dataset.",
                       seqan3::option_spec::required,
                       seqan3::input_file_validator{{"fa", "fasta"}});
-    parser.add_option(args.index_path,
-                      'o',
-                      "output",
-                      "The output index file path.",
-                      seqan3::option_spec::standard,
-                      seqan3::output_file_validator{seqan3::output_file_open_options::create_new, {"index"}});
+
+    parser.add_option(arguments.kmer_size,
+                      sharg::config{.short_id = '\0',
+                                    .long_id = "kmer",
+                                    .description = "The k-mer size.",
+                                    .validator = sharg::arithmetic_range_validator{1, 32}});
+    parser.add_option(arguments.window_size,
+                      sharg::config{.short_id = '\0',
+                                    .long_id = "window",
+                                    .description = "The window size.",
+                                    .default_message = "k-mer size",
+                                    .hidden = arguments.is_socks,
+                                    .validator = positive_integer_validator{}});
+
+
+    parser.add_option(
+        arguments.out_path,
+        sharg::config{.short_id = '\0',
+                      .long_id = "output",
+                      .description =
+                          arguments.is_socks
+                              ? "Provide an output filepath."
+                              : "Provide an output filepath or an output directory if --compute-minimiser is used.",
+                      .required = true});
+
 }
 
 //![main]
 int main(int argc, char const ** argv)
 {
-    seqan3::argument_parser parser("Indexer", argc, argv);
+    sharg::parser parser("Mini-App", argc, argv);
     cmd_arguments args{};
 
     initialise_argument_parser(parser, args);
 
+    // Parse the given arguments and catch possible errors.
     try
     {
-        parser.parse();
+        parser.parse();                                             // trigger command line parsing
     }
-    catch (seqan3::argument_parser_error const & ext)
+    catch (sharg::parser_error const & ext)                         // catch user errors
     {
-        std::cerr << "[PARSER ERROR] " << ext.what() << '\n';
+        seqan3::debug_stream << "[Error] " << ext.what() << '\n';   // customise your error message
         return -1;
     }
+
+    // Here you could do further checks for given parameters.
 
     run_program(args.reference_path, args.index_path);
 
